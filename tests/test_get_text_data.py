7ca120fbc3425e7d331d16282ebdb87670277a78
Anthony Wynne <a.wynne@svgc.co.uk>
2022-12-20 12:11:39 +0000

    try again to fail test

--- tests/test_get_text_data.py
+++ tests/test_get_text_data.py
@@ -1,6 +1,9 @@
 from src.get_text_data import get_wiki, get_random_wiki_page
 
+
 def test_get_wiki():
     assert len(get_wiki()) > 0
+
+
 def test_get_random_wiki_page():
-    assert len(get_random_wiki_page()) > 10
+    assert len(get_random_wiki_page()) > 10000
