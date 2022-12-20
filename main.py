85fd2eb8e793c78a00f9e3edfcd763afe72391bc
Anthony Wynne <a.wynne@svgc.co.uk>
2022-12-20 16:07:01 +0000

    fastapi working

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+# note does not run in jupyter notebook, run in terminal
 from fastapi import FastAPI
 import uvicorn
-
-# from src.get_text_data import get_wiki, get_random_wiki_page
+from src.get_text_data import get_wiki, get_random_wiki_page
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Duke"}
+    return {"message": "Template ML API to work with text data"}
 
 
-@app.get("/add/{num1}/{num2}")
-async def add(num1: int, num2: int):
-    """Add two numbers together"""
+@app.get("/wiki_summary/{page_name}")
+async def wiki_summary(page_name: str):
+    """Get the summary of a Wikipedia page"""
 
-    total = num1 + num2
-    return {"total": total}
+    result = get_wiki(page_name)
+    return {"summary": result}
+
+
+@app.get("/random_wiki_page")
+async def random_wiki_page():
+    """Get a random Wikipedia page summary"""
+
+    result = get_wiki(get_random_wiki_page()[0])
+    return {"summary": result}
 
 
 if __name__ == "__main__":
+    # goto localhost:8080/
+    # or localhost:8080/docs for the interactive docs
     uvicorn.run(app, port=8080, host="0.0.0.0")
+
