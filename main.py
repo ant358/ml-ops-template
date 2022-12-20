f662915c5b23b68fcc47c0dbb1c86ea1c57486b3
Anthony Wynne <a.wynne@svgc.co.uk>
2022-12-20 15:40:20 +0000

    added fast api

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-from src.get_text_data import get_wiki, get_random_wiki_page
+from fastapi import FastAPI
+import uvicorn
 
-print(get_wiki(get_random_wiki_page()[0]))
+# from src.get_text_data import get_wiki, get_random_wiki_page
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Duke"}
+
+
+@app.get("/add/{num1}/{num2}")
+async def add(num1: int, num2: int):
+    """Add two numbers together"""
+
+    total = num1 + num2
+    return {"total": total}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
